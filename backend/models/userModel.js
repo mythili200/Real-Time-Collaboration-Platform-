7092ddb9a7f7778b1e09effdc60d3614b351972f
Mythili <mythili4997@gmail.com>
2026-03-22 22:09:41 +0530

    added userModel

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    googleId: String,
+    avatar: String,
+  },
+  { timestamps: true },
+);
+
+userSchema.pre("save", async function () {
+  this.password = bcrypt.hash(this.password, 10);
+});
+export const userModel = mongoose.model("Users", userSchema);
