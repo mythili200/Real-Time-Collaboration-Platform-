a73de16305e95f52d25a6ad19a669fb227c85dce
Mythili <mythili4997@gmail.com>
2026-03-23 19:10:42 +0530

    saved real time data in db

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { messageModel } from "./models/messageModel.js";
 
 dotenv.config();
 connectDB();
@@ -15,23 +16,51 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("User Connected", socket.id);
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
 
-  socket.on("joinRoom", (roomId) => {
+  if (!token) return next(new Error("Unauthorized"));
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    socket.user = decoded;
+    next();
+  } catch (err) {
+    next(new Error("Invalid Token"));
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // ✅ Join Room + Load old messages
+  socket.on("joinRoom", async (roomId) => {
     socket.join(roomId);
-    console.log(`User joined room: ${roomId}`);
+
+    const messages = await messageModel
+      .find({ roomId })
+      .sort({ createdAt: 1 })
+      .limit(50);
+
+    socket.emit("loadMessages", messages); // send old messages only to this user
   });
 
-  // Send Message / Update
-  socket.on("sendMessage", ({ roomId, message }) => {
-    io.to(roomId).emit("receiveMessage", message);
+  //Send Message (SAVE + BROADCAST)
+  socket.on("sendMessage", async ({ roomId, message }) => {
+    const newMessage = await messageModel.create({
+      roomId,
+      message,
+      user: socket.user._id,
+    });
+
+    io.to(roomId).emit("receiveMessage", newMessage);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    console.log("User disconnected:", socket.id);
   });
 });
+
 
 server.listen(process.env.PORT, () => {
   console.log(`server is running on ${process.env.PORT} `);
